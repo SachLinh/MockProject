1bcca2d1c699bf6eee0df4b6ebfa4a60ab01b117
locz <loc.6.10.20000@gmail.com>
2022-03-22 11:13:43 +0700

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Content from './components/Home_TrangChu/Content';
-import Cart from './components/cart-order/Cart';
-import Header from './components/FooterAndHeader/Header';
+import Content from './Components/Home_TrangChu/Content';
+import Cart from './Components/cart-order/Cart';
+import Header from './Components/FooterAndHeader/Header';
 
 function App() {
 	return (
