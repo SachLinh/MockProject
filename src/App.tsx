0676d2f9c50c872d656e56d3dc23a4e0adeb97e0
SachLinh <sachlinh12345@gmail.com>
2022-03-22 07:56:42 +0700

    merge and Edit danh muc sp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-import Body from "./component2Tuan/Body";
+/** @format */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Body from './component2Tuan/Body';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Menu from './Components/Home_TrangChu/Menu';
 
 function App() {
-  return (
-   <div className='App'>
-    <Body/>
-      </div>;
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<div className='pt-[70px]'>
+				<Menu />
+				<div className='px-[75px] w-full h-[75px] mt-[15px] mb-[25px]'>
+					<Link to=''>
+						<img
+							src='https://cdn.cellphones.com.vn/media/wysiwyg/Banner/1200-75-max.png'
+							alt=''
+						/>
+					</Link>
+				</div>
+				<Body />
+				<Footer />
+			</div>
+		</div>
+	);
+}
 export default App;
