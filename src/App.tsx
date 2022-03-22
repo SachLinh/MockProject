63f60843e6697db922e0c38d9715d0e4270393e4
SachLinh <sachlinh12345@gmail.com>
2022-03-22 07:21:02 +0700

    Edit App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 /** @format */
 
 import React from 'react';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Menu from './Components/Home_TrangChu/Menu';
 
 function App() {
-	return (
-		<div className='App'>
-			<Header />
-			<div className='pt-[70px]'>
-				<Menu />
-				<Footer />
-			</div>
-		</div>
-	);
+	return <div className='App'></div>;
 }
 
 export default App;
