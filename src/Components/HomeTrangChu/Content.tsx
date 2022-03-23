c195bb21f9c6e8d134cc839f8c2efec32aa3f2aa
SachLinh <sachlinh12345@gmail.com>
2022-03-23 09:01:08 +0700

    SlideMenu

--- src/Components/HomeTrangChu/Content.tsx
+++ src/Components/HomeTrangChu/Content.tsx
@@ -10,11 +10,12 @@ export default function Content() {
 	return (
 		<div className='pt-[70px]'>
 			<Menu />
-			<div className='px-[75px] w-full h-[75px] mt-[15px] mb-[25px]'>
+			<div className='w-full px-[75px] h-[75px] mt-[15px] mb-[25px] flex flex-row justify-center items-center'>
 				<Link to=''>
 					<img
 						src='https://cdn.cellphones.com.vn/media/wysiwyg/Banner/1200-75-max.png'
 						alt=''
+						className='w-full'
 					/>
 				</Link>
 			</div>
--- src/Components/HomeTrangChu/Menu.tsx
+++ src/Components/HomeTrangChu/Menu.tsx
@@ -1,12 +1,19 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Autoplay, FreeMode, Navigation, Thumbs } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
 export default function Menu() {
+	const [thums, setThums] = useState<any>(null);
 	return (
 		<div className='w-full mt-[10px] mb-[20px] font-Roboto text-[14px] h-[376px]'>
 			<div className='px-[75px] flex flex-row justify-between w-full h-full'>
-				<div className='w-[205px] rounded-2xl shadow-[0_0px_5px_2px_#9b9a9a]'>
+				<div className='w-[20%] rounded-2xl shadow-[0_0px_5px_2px_#9b9a9a]'>
 					<ul className='w-full h-full flex flex-col justify-between '>
 						<li className='relative w-full hover:bg-[#d6d3d3]  p-[6px] first:rounded-t-2xl pl-[10px] flex flex-row justify-between items-center group'>
 							<Link className='w-full' to=''>
@@ -116,38 +123,85 @@ export default function Menu() {
 						</li>
 					</ul>
 				</div>
-				<div className='rounded-2xl shadow-[#9e9c9c] w-[700px] shadow-[0_0px_5px_2px_#9b9a9a]'>
-					<div className='w-full'>
-						<img
-							src='https://cdn.cellphones.com.vn/media/ltsoft/promotion/mI_12.png'
-							alt=''
-							className='w-full h-[304px] rounded-t-2xl'
-						/>
-					</div>
-					<ul className='w-full flex flex-row justify-between h-[72px] text-[12px] text-[#340a40] '>
-						<li className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6] hover:rounded-b-2xl'>
+				<div className='rounded-2xl shadow-[#9e9c9c] w-[55%] shadow-[0_0px_5px_2px_#9b9a9a]'>
+					<Swiper
+						className='w-full'
+						loop={true}
+						spaceBetween={10}
+						autoplay={{
+							delay: 2500,
+							disableOnInteraction: false,
+						}}
+						thumbs={{ swiper: thums }}
+						modules={[FreeMode, Navigation, Thumbs, Autoplay]}>
+						<SwiperSlide>
+							<img
+								src='https://cdn.cellphones.com.vn/media/ltsoft/promotion/A53.png'
+								alt=''
+								className='w-full h-[304px] rounded-t-2xl'
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<img
+								src='https://cdn.cellphones.com.vn/media/ltsoft/promotion/mI_12.png'
+								alt=''
+								className='w-full h-[304px] rounded-t-2xl'
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<img
+								src='https://cdn.cellphones.com.vn/media/ltsoft/promotion/reno7.png'
+								alt=''
+								className='w-full h-[304px] rounded-t-2xl'
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<img
+								src='https://cdn.cellphones.com.vn/media/ltsoft/promotion/ASUS-690x300_T3300_222.jpg'
+								alt=''
+								className='w-full h-[304px] rounded-t-2xl'
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<img
+								src='https://cdn.cellphones.com.vn/media/ltsoft/promotion/ipad_10.2_home.png'
+								alt=''
+								className='w-full h-[304px] rounded-t-2xl'
+							/>
+						</SwiperSlide>
+					</Swiper>
+					<Swiper
+						className='w-full flex flex-row justify-between h-[72px] text-[12px] text-[#340a40]'
+						onSwiper={setThums}
+						loop={true}
+						spaceBetween={10}
+						slidesPerView={4}
+						freeMode={true}
+						watchSlidesProgress={true}
+						modules={[FreeMode, Navigation, Thumbs]}>
+						<SwiperSlide className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6] hover:rounded-b-2xl active:border-b-2 active:border-b-black'>
 							<Link to=''>GALAXY A53</Link>
 							<p>Ưu đãi siêu khủng</p>
-						</li>
-						<li className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6] '>
+						</SwiperSlide>
+						<SwiperSlide className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6] '>
 							<Link to=''>XIAOMI 12 SERTIES</Link>
 							<p>Quà cực xịn</p>
-						</li>
-						<li className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6] '>
+						</SwiperSlide>
+						<SwiperSlide className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6] '>
 							<Link to=''>OPPO ZENO7 SERIES</Link>
 							<p>Đặt trước nhiều quà</p>
-						</li>
-						<li className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6] '>
+						</SwiperSlide>
+						<SwiperSlide className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6] '>
 							<Link to=''>VIVOBOOK 13 OLED</Link>
 							<p>Mở bán quà ngon</p>
-						</li>
-						<li className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6]  hover:rounded-b-2xl '>
+						</SwiperSlide>
+						<SwiperSlide className='w-full h-full flex flex-col justify-center items-center p-[2px] hover:bg-[#f3f4f6]  hover:rounded-b-2xl '>
 							<Link to=''>IPAD 12.2 2021</Link>
 							<p>Giá cực sốc</p>
-						</li>
-					</ul>
+						</SwiperSlide>
+					</Swiper>
 				</div>
-				<div className='flex flex-col justify-between items-start'>
+				<div className='w-[20%] flex flex-col justify-between items-start'>
 					<Link to=''>
 						<img
 							src='https://cdn.cellphones.com.vn/media/ltsoft/promotion/13_series_right_desk.png'
