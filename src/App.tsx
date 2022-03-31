029671a55aa7681e50169da6a76a7725e2271696
SachLinh <86608820+SachLinh@users.noreply.github.com>
2022-03-31 08:37:15 +0700

    Chỉnh lại App và addUser

--- src/App.tsx
+++ src/App.tsx
@@ -40,74 +40,44 @@ import AppUser from "./AppUser";
 function App() {
 
 
-<!-- 	const [isSignedIn, setIsSignedIn] = useState<boolean>(false); // Local signed-in state.
+// <!-- 	const [isSignedIn, setIsSignedIn] = useState<boolean>(false); // Local signed-in state.
 
-	// Listen to the Firebase Auth state and set the local state.
-	useEffect(() => {
-		const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
-			setIsSignedIn(!!user);
-			localStorage.setItem('mock-project-signed-in', JSON.stringify(isSignedIn))
-			localStorage.setItem('mock-project-smember', JSON.stringify(user))
-		});
-		return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
-	}, []);
+// 	// Listen to the Firebase Auth state and set the local state.
+// 	useEffect(() => {
+// 		const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+// 			setIsSignedIn(!!user);
+// 			localStorage.setItem('mock-project-signed-in', JSON.stringify(isSignedIn))
+// 			localStorage.setItem('mock-project-smember', JSON.stringify(user))
+// 		});
+// 		return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+// 	}, []);
 
-	return (
-		<div className='App w-full flex flex-col'>
-			<Header />
-			<Routes>
-				<Route path='/' element={<Content />}></Route>
-				<Route path='/Admin' element={<Admin />}></Route>
-				<Route path='/Smember' element={<Smember />}></Route>
-				<Route path='/Smember/thong-tin' element={<MemberInfo />}></Route>
-				<Route path='/cart' element={<Cart />}></Route>
-				<Route path='/payment' element={<Payment />}></Route>
-				<Route path='/payment-info' element={<PaymentInfo />}></Route>
-				<Route path='/complete-payment' element={<CompletePayment />}></Route>
-				<Route path='/SearchOrder' element={<SearchOrder />}></Route>
-				<Route path='/DanhMucSP/:idDM' element={<DanhMucSP />}></Route>
-				<Route path='/DanhMucSPCon/:idDM/Loai/:idLoai' element={<DanhMucSPCon />}></Route>
-				<Route path='/product-detail/:idDM/Loai/:idLoai/SanPham/:idSP' element={<ProductDetail />}></Route>
-			</Routes>
-			<Footer />
-		</div>
-	);
-======= -->
+// 	return (
+// 		<div className='App w-full flex flex-col'>
+// 			<Header />
+// 			<Routes>
+// 				<Route path='/' element={<Content />}></Route>
+// 				<Route path='/Admin' element={<Admin />}></Route>
+// 				<Route path='/Smember' element={<Smember />}></Route>
+// 				<Route path='/Smember/thong-tin' element={<MemberInfo />}></Route>
+// 				<Route path='/cart' element={<Cart />}></Route>
+// 				<Route path='/payment' element={<Payment />}></Route>
+// 				<Route path='/payment-info' element={<PaymentInfo />}></Route>
+// 				<Route path='/complete-payment' element={<CompletePayment />}></Route>
+// 				<Route path='/SearchOrder' element={<SearchOrder />}></Route>
+// 				<Route path='/DanhMucSP/:idDM' element={<DanhMucSP />}></Route>
+// 				<Route path='/DanhMucSPCon/:idDM/Loai/:idLoai' element={<DanhMucSPCon />}></Route>
+// 				<Route path='/product-detail/:idDM/Loai/:idLoai/SanPham/:idSP' element={<ProductDetail />}></Route>
+// 			</Routes>
+// 			<Footer />
+// 		</div>
+// 	);
+// ======= -->
   return (
        <Routes>
         <Route path='/*' element={<AppUser />} />
         <Route path='/Admin/*' element={<Admin />} />
       </Routes>
-    // <div>
-    //   <div className="App w-full flex flex-col">
-    //     <Header />
-
-    //     <Routes>
-    //       <Route path="/" element={<Content />}></Route>
-    //       <Route path="/Admin" element={<Admin />}></Route>
-    //       <Route path="/QuanLyDanhMuc" element={<QuanLyDanhMuc />}>
-    //       </Route>
-    //       <Route path="/Update/:idDM" element={<UpdateDanhMuc />}>
-    //         </Route>
-    //       <Route path="/cart" element={<Cart />}></Route>
-    //       <Route path="/payment" element={<Payment />}></Route>
-    //       <Route path="/CuaHang" element={<CuaHang />}></Route>
-    //       <Route path="/payment-info" element={<PaymentInfo />}></Route>
-    //       <Route path="/complete-payment" element={<CompletePayment />}></Route>
-    //       <Route path="/SearchOrder" element={<SearchOrder />}></Route>
-    //       <Route path="/DanhMucSP/:idDM" element={<DanhMucSP />}></Route>
-    //       <Route
-    //         path="/DanhMucSPCon/:idDM/Loai/:idLoai"
-    //         element={<DanhMucSPCon />}
-    //       ></Route>
-    //       <Route
-    //         path="/product-detail/:idDM/Loai/:idLoai/SanPham/:idSP"
-    //         element={<ProductDetail />}
-    //       ></Route>
-    //     </Routes>
-    //     <Footer />
-    //   </div>
-    // </div>
   );
 }
 export default App;
